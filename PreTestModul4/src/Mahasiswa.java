4c5ee3969f76a59b4472262f40ac78e60b63906a
Naufal-strong <naufalfakhrianto20@gmail.com>
2026-03-31 15:15:14 +0700

    Modul4

---
+++ PreTestModul4/src/Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+
+    String nama;
+    int umur;
+    String jurusanStudi;
+
+    public Mahasiswa(String nama, int umur, String jurusanStudi) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusanStudi = jurusanStudi;
+    }
+}    
